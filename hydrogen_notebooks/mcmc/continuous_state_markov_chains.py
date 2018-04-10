c85e3ad90fc45eecd5b8b3486a2244dc16e8b019
Troy Stribling <troy.stribling@gmail.com>
2018-04-10 08:34:36 -0700

    progress on equiblibrium plots. started playing with models

--- hydrogen_notebooks/mcmc/continuous_state_markov_chains.py
+++ hydrogen_notebooks/mcmc/continuous_state_markov_chains.py
@@ -23,21 +23,20 @@ def ar_1_series(α, σ, x0, nsample=100):
     return samples
 
 def ar_1_kernel(α, σ, x, y):
-    p = numpy.zeros(npts)
-    for i in range(0, npts):
+    p = numpy.zeros(len(y))
+    for i in range(0, len(y)):
         ε  = ((y[i] -  α * x)**2) / ( 2.0 * σ**2)
-        p[i] = numpy.exp(ε) / numpy.sqrt(2 * numpy.pi * σ**2)
+        p[i] = numpy.exp(-ε) / numpy.sqrt(2 * numpy.pi * σ**2)
     return p
 
 # %%
 
 α = 0.5
-σ = 0.5
-x0 = 1.0
+σ = 1.0
 npts = 200
-nsample = 10
+nsample = 50
 
-nplot = 2
+nplot = 1
 nplots = int(nsample / nplot)
 n = 0
 np = 0
@@ -47,7 +46,9 @@ ymin = -ymax
 dy = (ymax - ymin) / npts
 y = [ymin + dy * i for i in range(0, npts)]
 
-alpha_min = 0.5
+p = numpy.zeros(npts)
+
+alpha_min = 0.1
 alpha_max = 1.0
 dalpha = (alpha_max - alpha_min) / nplots
 alpha = [alpha_min + dalpha * i for i in range(0, nplots)]
@@ -58,9 +59,38 @@ axis.set_ylabel(r'$\pi$')
 axis.set_title("AR(1) Relaxation to Equilibrium")
 axis.grid(True, zorder=5)
 
-for x in ar_1(α, σ, x0, nsample):
+for x in ar_1_series(α, σ, 5.0, nsample):
     p += ar_1_kernel(α, σ, x, y)
     if n % nplot == 0:
-        axis.plot(y, p / (n + 1), color="#A60628", lw="3", zorder=10, alpha=alpha[np])
+        axis.plot(y, p / (n + 1), color="#1E90FF", lw="3", zorder=10, alpha=alpha[np])
         np += 1
     n += 1
+
+
+# %%
+
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("y")
+axis.set_ylabel(r'$\pi$')
+axis.set_title("AR(1) Relaxation to Equilibrium")
+axis.grid(True, zorder=5)
+
+n = 0
+np = 0
+for x in ar_1_series(α, σ, -5.0, nsample):
+    p += ar_1_kernel(α, σ, x, y)
+    if n % nplot == 0:
+        axis.plot(y, p / (n + 1), color="#1E90FF", lw="3", zorder=10, alpha=alpha[np])
+        np += 1
+    n += 1
+
+# %%
+
+samples = ar_1_series(α, σ, x0, 10000)
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("Value")
+axis.set_ylabel(r'$\pi_E$')
+axis.set_title("Equilbrium PDF Comparison")
+axis.grid(True, zorder=5)
+_, x_values, _ = axis.hist(samples, 50, density=True, color="#348ABD", alpha=0.6, edgecolor="#348ABD", label=f"Sampled Density", lw="3", zorder=10)
+axis.legend()
