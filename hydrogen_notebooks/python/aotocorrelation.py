db11a94da287a157f28cf4f153c2398513038fd6
Troy Stribling <troy.stribling@gmail.com>
2018-05-21 08:01:30 -0700

    starting work on autocorrelation

---
+++ hydrogen_notebooks/python/aotocorrelation.py
@@ -0,0 +1,10 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+
+import numpy
+from matplotlib import pyplot
+from glyfish import config
+from scipy import fftpack
+
+pyplot.style.use(config.glyfish_style)
