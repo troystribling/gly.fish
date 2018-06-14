58fad1b50f7b433500a43ff341f0d0e076609b26
Troy Stribling <troy.stribling@gmail.com>
2018-06-14 08:44:59 -0700

    first pass at change point gibbs sampler

--- hydrogen_notebooks/mcmc/gibbs_sampling/change_point_model.py
+++ hydrogen_notebooks/mcmc/gibbs_sampling/change_point_model.py
@@ -39,26 +39,70 @@ def change_point_df_cdf(counts, λ1, λ2):
     cdf = numpy.cumsum(df)
     return df, cdf
 
-def change_point_example_df(counts, λ1, λ2):
-    ncounts = len(counts)
-    mult_n = numpy.zeros(ncounts)
-    for i in range(ncounts):
-        mult_n[i]=numpy.sum(counts[0:i])*numpy.log(λ1)-i*λ1+numpy.sum(counts[i:ncounts])*numpy.log(λ2)-(ncounts-i)*λ2
-    return numpy.exp(mult_n-max(mult_n))
+def change_point_inverse_cdf_sample(counts, λ1, λ2):
+    ndf, ncdf = change_point_df_cdf(counts, λ1, λ2)
+    return numpy.nonzero(ncdf <= numpy.random.rand())[0][-1]
 
-def change_point_samples_example(counts, λ1, λ2):
-    ncounts = len(counts)
-    mult_n = numpy.zeros(ncounts)
-    for i in range(ncounts):
-	       mult_n[i]=numpy.sum(counts[0:i])*numpy.log(λ1)-i*λ1+numpy.sum(counts[i:ncounts])*numpy.log(λ2)-(ncounts-i)*λ2
-    mult_n=numpy.exp(mult_n-max(mult_n))
-    return numpy.where(multinomial(1,mult_n/sum(mult_n),size=1)==1)[1][0]
+def change_point_multinomial_sample(counts, λ1, λ2):
+    ndf, ncdf = change_point_df_cdf(counts, λ1, λ2)
+    return numpy.where(numpy.random.multinomial(1, ndf, size=1)==1)[1][0]
+
+def gibbs_sample(counts, n0, λ10, λ20, α, β, nsample):
+    n = numpy.zeros(nsample)
+    λ1 = numpy.zeros(nsample)
+    λ2 = numpy.zeros(nsample)
+    n[0] = n0
+    λ1[0] = λ10
+    λ2[0] = λ20
+    for i in range(1, nsample):
+        λ1[i] = stats.gamma.rvs(α, scale=1.0/β)
+        λ2[i] = stats.gamma.rvs(α, scale=1.0/β)
+        n[i] = change_point_inverse_cdf_sample(counts, λ1[i], λ2[i])
+    return n, λ1, λ2
 
 # %%
 
-
 ncounts = 101
-n, λ1, λ2, counts = generate_counts_time_series(ncounts, 2, 1)
+α = 2
+β = 1
+
+# %%
+
+nplot = 4
+
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("n")
+axis.set_xlim([0, ncounts-1])
+axis.set_ylim([0, 1.0])
+axis.set_ylabel("Probability")
+axis.set_title("Change Point Model")
+
+for i in range(nplot):
+    n, λ1, λ2, counts = generate_counts_time_series(ncounts, α, β)
+    ndf, ncdf = change_point_df_cdf(counts, λ1, λ2)
+    axis.plot(range(ncounts), ndf, label="Distribution")
+
+axis.legend()
+
+# %%
+
+n, λ1, λ2, counts = generate_counts_time_series(ncounts, α, β)
+ndf, ncdf = change_point_df_cdf(counts, λ1, λ2)
+title = f"Change Point Model"+r", $λ_1=$"+f"{format(λ1, '2.2f')}"+r", $λ_2=$"+f"{format(λ2, '2.2f')}, n={n}"
+
+figure, axis = pyplot.subplots(figsize=(12, 5))
+axis.set_xlabel("n")
+axis.set_xlim([0, ncounts-1])
+axis.set_ylim([0, 1.0])
+axis.set_ylabel("Probability")
+axis.set_title(title)
+axis.plot(range(ncounts), ndf, label="Distribution")
+axis.plot(range(ncounts), ncdf, label="CDF")
+axis.legend()
+
+# %%
+
+n, λ1, λ2, counts = generate_counts_time_series(ncounts, α, β)
 
 # %%
 
@@ -85,27 +129,7 @@ axis.legend()
 # %%
 
 ndf, ncdf = change_point_df_cdf(counts, λ1, λ2)
-title = f"Change Point Model"+r", $λ_1=$"+f"{format(λ1, '2.2f')}"+r", $λ_2=$"+f"{format(λ2, '2.2f')}, n={n}"
+numpy.random.multinomial(1, ndf, size=1)
 
-figure, axis = pyplot.subplots(figsize=(12, 5))
-axis.set_xlabel("n")
-axis.set_xlim([0, ncounts-1])
-axis.set_ylim([0, numpy.max(ndf)])
-axis.set_ylabel("Probability")
-axis.set_title(title)
-axis.plot(range(ncounts), ndf)
-axis.axvline(x=n, color="#A60628")
 
 # %%
-
-ndf = change_point_example_df(counts, λ1, λ2)
-title = f"Change Point Model"+r", $λ_1=$"+f"{format(λ1, '2.2f')}"+r", $λ_2=$"+f"{format(λ2, '2.2f')}, n={n}"
-
-figure, axis = pyplot.subplots(figsize=(12, 5))
-axis.set_xlabel("n")
-axis.set_xlim([0, ncounts-1])
-axis.set_ylim([0, numpy.max(ndf)])
-axis.set_ylabel("Probability")
-axis.set_title(title)
-axis.plot(range(ncounts), ndf)
-axis.axvline(x=n, color="#A60628")
