32e7875ae58999e4eec36813dce9b7d40262e901
Troy Stribling <troy.stribling@gmail.com>
2018-07-28 14:25:52 -0700

    make plot symbols consistent with post

--- hydrogen_notebooks/mcmc/rejection_sampling.py
+++ hydrogen_notebooks/mcmc/rejection_sampling.py
@@ -36,9 +36,10 @@ def plot_sampled_pdf(title, f, samples, plot_name):
     axis.legend(bbox_to_anchor=(0.3, 0.85))
     config.save_post_asset(figure, "rejection_sampling", plot_name)
 
-def acceptance_plot(title, h, x_samples, nsamples, ymax, xmax, legend_loc, plot_name):
+def acceptance_plot(title, h, y_samples, ymax, xmax, legend_loc, plot_name):
+    nsamples = len(y_samples)
     x_values = numpy.linspace(0.0, xmax, 500)
-    samples, y_samples, accepted_mask = rejection_sample(h, x_samples, ymax)
+    samples, u, accepted_mask = rejection_sample(h, y_samples, ymax)
     rejected_mask = numpy.logical_not(accepted_mask)
     efficiency = 100.0 * (len(samples) / nsamples)
     figure, axis = pyplot.subplots(figsize=(10, 6))
@@ -47,8 +48,8 @@ def acceptance_plot(title, h, x_samples, nsamples, ymax, xmax, legend_loc, plot_
     axis.set_yticks([0.2, 0.4, 0.6, 0.8, 1.0])
     axis.set_xlim([0.0, 1.6])
     axis.plot(x_values, h(x_values) / ymax, zorder=5, lw=3, color="#5600C9")
-    axis.scatter(x_samples[accepted_mask], y_samples[accepted_mask], label="Accepted Samples", alpha=0.5, zorder=5)
-    axis.scatter(x_samples[rejected_mask], y_samples[rejected_mask], label="Rejected Samples", alpha=0.4, zorder=5)
+    axis.scatter(y_samples[accepted_mask], u[accepted_mask], label="Accepted Samples", alpha=0.5, zorder=5)
+    axis.scatter(y_samples[rejected_mask], u[rejected_mask], label="Rejected Samples", alpha=0.4, zorder=5)
     axis.legend(bbox_to_anchor=legend_loc, framealpha=0.9, edgecolor="#DDDDDD")
     config.save_post_asset(figure, "rejection_sampling", plot_name)
 
@@ -119,12 +120,33 @@ config.save_post_asset(figure, "rejection_sampling", "weibull_pdf")
 
 # %%
 
+x_values = numpy.linspace(0.0, xmax, 500)
+
+target_pdf = weibull_pdf
+proposal_pdf = 1.0/xmax
+target_label = r"$f_X(y)=Weibull(k={k}, λ={λ})$".format(k=k, λ=λ)
+proposal_label = r"$f_Y(y)=Uniform(0, {xmax})$".format(xmax=xmax)
+
+h = lambda x: target_pdf(x) * xmax
+figure, axis = pyplot.subplots(figsize=(10, 6))
+axis.set_ylim([0, 3.75])
+axis.set_yticks([0.5, 1.5, 2.5, 3.5])
+axis.set_xlim([0.0, 1.6])
+axis.set_title("Rejection Sampling Functions")
+axis.plot(x_values, target_pdf(x_values), label=target_label)
+axis.plot(x_values, numpy.full(len(x_values), 1.0/xmax), label=proposal_label)
+axis.plot(x_values, h(x_values), label=r"$h(y)=f_X(y)/f_Y(y)$")
+axis.legend(bbox_to_anchor=(0.52, 0.7), framealpha=0.0)
+config.save_post_asset(figure, "rejection_sampling", "weibull_uniform_sampled_functions")
+
+# %%
+
 xmax = 1.6
 ymax = 2.0
 nsamples = 100000
-x_samples = numpy.random.rand(nsamples) * xmax
+y_samples = numpy.random.rand(nsamples) * xmax
 
-samples, _, _ = rejection_sample(weibull_pdf, x_samples, ymax)
+samples, _, _ = rejection_sample(weibull_pdf, y_samples, ymax)
 
 title = f"Sampled Weibull Density, k={k}, λ={λ}, Uniform Proposal"
 plot_sampled_pdf(title, weibull_pdf, samples, "weibull_uniform_sampled_distribution")
@@ -134,9 +156,9 @@ plot_sampled_pdf(title, weibull_pdf, samples, "weibull_uniform_sampled_distribut
 xmax = 1.6
 ymax = 2.0
 nsamples = 10000
-x_samples = numpy.random.rand(nsamples) * xmax
+y_samples = numpy.random.rand(nsamples) * xmax
 title = f"Sampled Weibull Density, Uniform Proposal"
-acceptance_plot(title, weibull_pdf, x_samples, nsamples, ymax, xmax, (0.4, 0.75), "weibull_uniform_efficiency")
+acceptance_plot(title, weibull_pdf, y_samples, ymax, xmax, (0.4, 0.75), "weibull_uniform_efficiency")
 
 # %%
 
@@ -158,8 +180,8 @@ sigma_convergence(title, samples, σ, "weibull_uniform_sigma_convergence")
 x_values = numpy.linspace(0.0, 2.0, 500)
 target_pdf = weibull_pdf
 proposal_pdf = normal(μ, σ)
-target_label = r"$f(x)=Weibull(k={k}, λ={λ})$".format(k=k, λ=λ)
-proposal_label = r"$g(x)=Normal(μ={μ}, σ={σ})$".format(μ=μ, σ=σ)
+target_label = r"$f_X(y)=Weibull(k={k}, λ={λ})$".format(k=k, λ=λ)
+proposal_label = r"$f_Y(y)=Normal(μ={μ}, σ={σ})$".format(μ=μ, σ=σ)
 
 h = lambda x: target_pdf(x) / proposal_pdf(x)
 figure, axis = pyplot.subplots(figsize=(10, 6))
@@ -169,7 +191,7 @@ axis.set_xlim([0.0, 1.6])
 axis.set_title("Rejection Sampling Functions")
 axis.plot(x_values, target_pdf(x_values), label=target_label)
 axis.plot(x_values, proposal_pdf(x_values), label=proposal_label)
-axis.plot(x_values, h(x_values), label=r"$h(x)=f(x)/g(x)$")
+axis.plot(x_values, h(x_values), label=r"$h(y)=f_X(y)/f_Y(y)$")
 axis.legend(bbox_to_anchor=(0.95, 0.92))
 config.save_post_asset(figure, "rejection_sampling", "weibull_normal_1_sampled_functions")
 
@@ -177,11 +199,11 @@ config.save_post_asset(figure, "rejection_sampling", "weibull_normal_1_sampled_f
 
 xmax = 1.6
 nsamples = 100000
-x_samples = numpy.random.normal(μ, σ, nsamples)
+y_samples = numpy.random.normal(μ, σ, nsamples)
 h = lambda x: weibull_pdf(x) / normal(μ, σ)(x)
 ymax = h(x_values).max()
 
-samples, y_samples, accepted_mask = rejection_sample(h, x_samples, ymax)
+samples, _, accepted_mask = rejection_sample(h, y_samples, ymax)
 
 title = f"Weibull Density, k={k}, λ={λ}, Normal Proposal"
 plot_sampled_pdf(title, weibull_pdf, samples, "weibull_normal_1_sampled_distribution")
@@ -189,9 +211,9 @@ plot_sampled_pdf(title, weibull_pdf, samples, "weibull_normal_1_sampled_distribu
 # %%
 
 nsamples = 10000
-x_samples = numpy.random.normal(μ, σ, nsamples)
+y_samples = numpy.random.normal(μ, σ, nsamples)
 
-acceptance_plot(title, h, x_samples, nsamples, ymax, xmax, (0.6, 0.75), "weibull_normal_1_efficiency")
+acceptance_plot(title, h, y_samples, ymax, xmax, (0.6, 0.75), "weibull_normal_1_efficiency")
 
 # %%
 
@@ -205,19 +227,17 @@ mean_convergence(title, samples, μ, "weibull_normal_1_mean_convergence")
 title = r"Weibull Density, Normal Proposal, Cumulative σ convergence"
 sigma_convergence(title, samples, σ, "weibull_normal_1_sigma_convergence")
 
-
-
 # Normal proposal density rejection sampled function
 # %%
 
 σ = 0.25
 μ = 0.9
-x_values = numpy.linspace(0.0, 2.0, 500)
+x_values = numpy.linspace(0.0, xmax, 500)
 
 target_pdf = weibull_pdf
 proposal_pdf = normal(μ, σ)
-target_label = r"$f(x)=Weibull(k={k}, λ={λ})$".format(k=k, λ=λ)
-proposal_label = r"$g(x)=Normal(μ={μ}, σ={σ})$".format(μ=μ, σ=σ)
+target_label = r"$f_X(y)=Weibull(k={k}, λ={λ})$".format(k=k, λ=λ)
+proposal_label = r"$f_Y(y)=Normal(μ={μ}, σ={σ})$".format(μ=μ, σ=σ)
 
 h = lambda x: target_pdf(x) / proposal_pdf(x)
 figure, axis = pyplot.subplots(figsize=(10, 6))
@@ -227,7 +247,7 @@ axis.set_xlim([0.0, 1.6])
 axis.set_title("Rejection Sampling Functions")
 axis.plot(x_values, target_pdf(x_values), label=target_label)
 axis.plot(x_values, proposal_pdf(x_values), label=proposal_label)
-axis.plot(x_values, h(x_values), label=r"$h(x)=f(x)/g(x)$")
+axis.plot(x_values, h(x_values), label=r"$h(y)=f_X(y)/f_Y(y)$")
 axis.legend(bbox_to_anchor=(0.52, 0.7), framealpha=0.0)
 config.save_post_asset(figure, "rejection_sampling", "weibull_normal_3_sampled_functions")
 
@@ -235,11 +255,11 @@ config.save_post_asset(figure, "rejection_sampling", "weibull_normal_3_sampled_f
 
 xmax = 1.6
 nsamples = 100000
-x_samples = numpy.random.normal(μ, σ, nsamples)
+y_samples = numpy.random.normal(μ, σ, nsamples)
 h = lambda x: weibull_pdf(x) / normal(μ, σ)(x)
 ymax = h(x_values).max()
 
-samples, y_samples, accepted_mask = rejection_sample(h, x_samples, ymax)
+samples, _, accepted_mask = rejection_sample(h, y_samples, ymax)
 
 title = f"Weibull Density, k={k}, λ={λ}, Normal Proposal"
 plot_sampled_pdf(title, weibull_pdf, samples, "weibull_normal_3_sampled_distribution")
@@ -249,10 +269,10 @@ plot_sampled_pdf(title, weibull_pdf, samples, "weibull_normal_3_sampled_distribu
 xmax = 1.6
 nsamples = 10000
 ymax = h(x_values).max()
-x_samples = numpy.random.normal(μ, σ, nsamples)
+y_samples = numpy.random.normal(μ, σ, nsamples)
 title = f"Weibull Density, k={k}, λ={λ}, Normal Proposal"
 
-acceptance_plot(title, h, x_samples, nsamples, ymax, xmax, (0.4, 0.73), "weibull_normal_3_efficiency")
+acceptance_plot(title, h, y_samples, ymax, xmax, (0.4, 0.73), "weibull_normal_3_efficiency")
 
 # %%
 
