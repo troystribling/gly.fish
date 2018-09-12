7c05d36e179723ee5cf0dfa892ec69455ad5186e
Troy Stribling <troy.stribling@gmail.com>
2018-09-12 08:40:56 -0700

    finished first pass on color maps

--- glyfish/config.py
+++ glyfish/config.py
@@ -15,12 +15,11 @@ def draw(dot, post, plot):
     pygraphviz.AGraph(dot).draw(path, format='png', prog='dot')
     return Image(pygraphviz.AGraph(dot).draw(format='png', prog='dot'))
 
+style_file = os.path.join(os.getcwd(), 'gly.fish.mplstyle')
+glyfish_style = pathlib.Path(style_file).as_uri()
+post_asset_path = os.path.join(os.getcwd(), 'assets', 'posts')
+
 color = matplotlib.colors.ColorConverter().to_rgb
-
-color("#329EFF")
-color("#FFE800")
-color("#FF9500")
-
 
 histogram_color_map_cdict = {'red':   ((0.0,  1.0, 1.0),
                                        (0.5,  0.19, 0.19),
@@ -39,14 +38,15 @@ histogram_color_map_cdict = {'red':   ((0.0,  1.0, 1.0),
                              }
 histogram_color_map = matplotlib.colors.LinearSegmentedColormap('HistogramMap', histogram_color_map_cdict)
 
-style_file = os.path.join(os.getcwd(), 'gly.fish.mplstyle')
-glyfish_style = pathlib.Path(style_file).as_uri()
-post_asset_path = os.path.join(os.getcwd(), 'assets', 'posts')
+alternate_color_map_colors = [color('white'), color("#8C35FF"),  color("#329EFF"), color("#FF9500"), color("#FFE800")]
+alternate_color_map =  matplotlib.colors.LinearSegmentedColormap.from_list('AlternateMap', alternate_color_map_colors, N=100 )
 
 contour_color_map = matplotlib.colors.ListedColormap(["#0067C4", "#FFE800", "#320075", "#FF9500",
                                                       "#329EFF", "#AC9C00", "#5600C9", "#FFC574",
                                                       "#003B6F", "#FFEB22", "#8C35FF", "#AC6500"])
-distribution_sample_cycler = cycler("color", ["#329EFF", "#320075"])
+alternate_contour_color_map = matplotlib.colors.ListedColormap(["#320075", "#FFEB22", "#FFC574", "#0067C4"])
+
+distribution_sample_cycler = cycler("color", ["#329EFF", "#FFEB22"])
 alternate_cycler = cycler("color", ["#0067C4", "#8C35FF", "#FF9500", "#FFE800", "#329EFF", "#FFC574", "#320075"])
 bar_plot_colors = ["#0067C4", "#FF9500", "#320075", "#FFE800", "#329EFF", "#FFC574", "#8C35FF"]
 bar_plot_cycler = cycler("color", bar_plot_colors)
