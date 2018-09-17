663fc0b2026660525576d7cc36914f83d8924dac
Troy Stribling <troy.stribling@gmail.com>
2018-09-17 08:36:13 -0700

    another gobbs samplng example

---
+++ hydrogen_notebooks/mcmc/gibbs_sampling/am207_bivariate_normals.py
@@ -0,0 +1,15 @@
+# %%
+
+%load_ext autoreload
+%autoreload 2
+
+import numpy
+
+from matplotlib import pyplot
+import scipy
+from glyfish import config
+from glyfish import stats
+
+%matplotlib inline
+
+pyplot.style.use(config.glyfish_style)
