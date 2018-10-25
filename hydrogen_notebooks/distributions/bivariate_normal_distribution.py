52fcd3ddd88464c52c7025d4e3a2fa1bf6b19edf
Troy Stribling <troy.stribling@gmail.com>
2018-10-25 13:14:37 -0700

    added contour value calcaulation

--- hydrogen_notebooks/distributions/bivariate_normal_distribution.py
+++ hydrogen_notebooks/distributions/bivariate_normal_distribution.py
@@ -13,7 +13,7 @@ pyplot.style.use(config.glyfish_style)
 
 # %%
 
-def bivariate_normal_pdf(μ1, μ2, σ1, σ2, ρ):
+def pdf(μ1, μ2, σ1, σ2, ρ):
     def f(x1, x2):
         y1 = (x1 - μ1) / σ1
         y2 = (x2 - μ2) / σ2
@@ -22,14 +22,7 @@ def bivariate_normal_pdf(μ1, μ2, σ1, σ2, ρ):
         return numpy.exp(-ε) / c
     return f
 
-def bivariate_normal_pdf_iso(μ1, μ2, σ1, σ2, ρ):
-    def f(x1, x2):
-        y1 = (x1 - μ1) / σ1
-        y2 = (x2 - μ2) / σ2
-        return (y1**2 + y2**2 - ρ * y1 * y2) / (2.0 * (1.- ρ**2))
-    return f
-
-def bivariate_normal_conditional_pdf(μ1, μ2, σ1, σ2, ρ):
+def conditional_pdf_y1_y2(μ1, μ2, σ1, σ2, ρ):
     def f(x1, x2):
         y1 = (x1 - μ1)
         y2 = (x2 - μ2)
@@ -42,10 +35,34 @@ def normal(x, σ=1.0, μ=0.0):
     ε = (x - μ)**2/(2.0*σ**2)
     return numpy.exp(-ε)/numpy.sqrt(2.0*numpy.pi*σ**2)
 
-def bivariate_normal_conditional_pdf_generator(y, μ1, μ2, σ1, σ2, ρ):
+def conditional_pdf_generator(y, μ1, μ2, σ1, σ2, ρ):
     loc = μ1 + ρ * σ1 * (y - μ2) / σ2
     scale = numpy.sqrt((1.0 - ρ**2) * σ1**2)
     return numpy.random.normal(loc, scale)
+
+def pdf_contour(μ1, μ2, σ1, σ2, ρ):
+    def f(θ, c):
+        y1 = c * σ1 * (numpy.sin(θ) + numpy.cos(θ) * ρ / numpy.sqrt(1.0 - ρ**2)) + μ1
+        y1 = c * σ2 * numpy.cos(θ) / numpy.sqrt(1.0 - ρ**2) + μ2
+        return (y1, y2)
+    return f
+
+def pdf_transform_y1_constant(μ1, μ2, σ1, σ2, ρ, c1):
+    def f(y2):
+        x1 = (c1 - μ1) / σ1
+        x2 = ((σ2 * ρ)(c1 - μ1) / σ1 - (y2 - μ2)) / (σ2 * numpy.sqrt(1.0 - ρ**2))
+        return (x1, x2)
+    return f
+
+def pdf_transform_y2_constant(μ1, μ2, σ1, σ2, ρ, c2):
+    def f(y1):
+        x1 = (y1 - μ1) / σ1
+        x2 = ((σ2 * ρ)(y1 - μ1) / σ1 - (c2 - μ2)) / (σ2 * numpy.sqrt(1.0 - ρ**2))
+        return (x1, x2)
+    return f
+
+def pdf_contour_constant(μ1, μ2, σ1, σ2, ρ, v):
+    return numpy.sqrt(2.0 * (1.0 - ρ**2) * numpy.log(numpy.pi * σ1 * σ2 * v * numpy.sqrt(1.0 - ρ**2)))
 
 # %%
 
@@ -69,7 +86,7 @@ for i in numpy.arange(npts):
         f_x1_x2[i, j] = pdf(x1_grid[i,j], y1_grid[i,j])
 
 # %%
-\
+
 figure, axis = pyplot.subplots(figsize=(10, 7))
 axis.set_xlabel("x")
 axis.set_ylabel("y")
@@ -81,6 +98,7 @@ config.save_post_asset(figure, "bivariate_normal_distribution", "bivariate_pdf_c
 # %%
 
 figure, axis = pyplot.subplots(figsize=(10, 7))
+axis.set_title(f"Bivariate Normal PDF: ρ={ρ}")
 axis.set_yticklabels([])
 axis.set_xticklabels([])
 
@@ -94,3 +112,15 @@ axis_z.plot_wireframe(x1_grid, y1_grid, f_x1_x2, rstride=25, cstride=25)
 config.save_post_asset(figure, "bivariate_normal_distribution", "bivariate_pdf_surface")
 
 # %%
+
+σ1 = 1.0
+σ2 = 1.0
+μ1 = 0.0
+μ2 = 0.0
+npts = 500
+ρ = 0.0
+θ = numpy.linspace(0.0, 2.0 * numpy.pi, npts)
+
+c = [0.0, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15]
+for n in range(len(c)):
+    f = bivariate_normal_pdf_contour(μ1, μ2, σ1, σ2, ρ)
