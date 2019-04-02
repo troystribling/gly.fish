1ef0e3c5ba16c2893f0246fb6a7c72f936ecc585
Troy Stribling <troy.stribling@gmail.com>
2019-04-01 20:45:46 -0700

    added hmc plots

--- hydrogen_notebooks/mcmc/hamiltonian_monte_carlo/gaussian_distribution.py
+++ hydrogen_notebooks/mcmc/hamiltonian_monte_carlo/gaussian_distribution.py
@@ -115,3 +115,64 @@ def HMC(U, K, dUdq, nsample, ε, nintegrate):
         H[j] = U(current_q) + K(current_p)
 
     return H, pall, qall, accept
+
+# %%
+
+def canonical_distribution(kinetic_energy, potential_energy):
+    def f(p, q):
+        return numpy.exp(-kinetic_energy(p) - potential_energy(q))
+    return f
+
+def canonical_distribution_mesh(kinetic_energy, potential_energy, npts):
+    x1 = numpy.linspace(-3.0, 3.0, npts)
+    x2 = numpy.linspace(-3.0, 3.0, npts)
+    f = canonical_distribution(kinetic_energy, potential_energy)
+    x1_grid, x2_grid = numpy.meshgrid(x1, x2)
+    f_x1_x2 = numpy.zeros((npts, npts))
+    for i in numpy.arange(npts):
+        for j in numpy.arange(npts):
+            f_x1_x2[i, j] = f(x1_grid[i,j], x2_grid[i,j])
+    return (x1_grid, x2_grid, f_x1_x2)
+
+def canonical_distribution_contour_plot(kinetic_energy, potential_energy, contour_values, title, plot_name):
+    npts = 500
+    x1_grid, x2_grid, f_x1_x2 = canonical_distribution_mesh(kinetic_energy, potential_energy, npts)
+    figure, axis = pyplot.subplots(figsize=(8, 8))
+    axis.set_xlabel(r"$q$")
+    axis.set_ylabel(r"$p$")
+    axis.set_xlim([-3.2, 3.2])
+    axis.set_ylim([-3.2, 3.2])
+    axis.set_title(title)
+    contour = axis.contour(x1_grid, x2_grid, f_x1_x2, contour_values, cmap=config.contour_color_map)
+    axis.clabel(contour, contour.levels[::2], fmt="%.3f", inline=True, fontsize=15)
+    config.save_post_asset(figure, "hamiltonian_monte_carlo", plot_name)
+
+def hamiltons_equations_integration_plot(kinetic_energy, potential_energy, contour_values, p, q, title, plot_name):
+    npts = 500
+    x1_grid, x2_grid, f_x1_x2 = canonical_distribution_mesh(kinetic_energy, potential_energy, npts)
+    figure, axis = pyplot.subplots(figsize=(8, 8))
+    axis.set_xlabel(r"$q$")
+    axis.set_ylabel(r"$p$")
+    axis.set_xlim([-3.2, 3.2])
+    axis.set_ylim([-3.2, 3.2])
+    axis.set_title(title)
+    contour = axis.contour(x1_grid, x2_grid, f_x1_x2, contour_values, cmap=config.contour_color_map)
+    axis.clabel(contour, contour.levels[::2], fmt="%.3f", inline=True, fontsize=15)
+    axis.plot(q, p, lw=1, alpha=0.3, color="#000000")
+    axis.plot(q[0], p[0], marker='o', color="#FF9500", markersize=13.0, label="Start")
+    axis.plot(q[-1], p[-1], marker='o', color="#320075", markersize=13.0, label="End")
+    axis.legend()
+    config.save_post_asset(figure, "hamiltonian_monte_carlo", plot_name)
+
+# %%
+
+canonical_distribution_contour_plot(K, U, [0.02, 0.1, 0.2, 0.4, 0.6, 0.8], "Canonical Distribution", "gaussian_potential_energy")
+
+# %%
+
+ε = 0.05
+nsteps = 500
+
+p, q = euler(-1.0, 1.0, nsteps, ε)
+title = f"Hamilton's Equations (Euler Method): Δt={ε}, nsteps={nsteps}"
+hamiltons_equations_integration_plot(K, U, [0.37], p, q, title, "hamiltons_equations_integration_euler_method_01_1000")
